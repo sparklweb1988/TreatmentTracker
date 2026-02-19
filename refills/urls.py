74996479e1e7a31db67433ec3bd8431a23f7d3e5
sparklweb1988 <sparklewebacademy@gmail.com>
2026-02-19 22:35:28 +0100

    Update urls.py

--- refills/urls.py
+++ refills/urls.py
@@ -2,8 +2,8 @@ from django.urls import path, re_path  # Import re_path for regex matching
 from . import views
 urlpatterns = [
     path('', views.signin_view, name='login'),
-    path('logout', views.logout_view, name='logout'),
-    path('dashboard/, views.dashboard, name='dashboard'),
+    path('logout/', views.logout_view, name='logout'),
+    path('dashboard/', views.dashboard, name='dashboard'),
     path('refills/', views.refill_list, name='refill_list'),
     path('refills/add/', views.refill_create, name='refill_add'),
     path('refills/edit/<int:pk>/', views.refill_update, name='refill_edit'),
